c9563b4caa7233f4454e15c9d675300287d1e61f
ikalwani <ikalwani@umich.edu>
2024-07-14 23:34:29 +0100

    Potfolio

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const rotatingText = document.getElementById("rotating-text");
-const texts = ["Software Engineering", "Web development", "Constantly Growing", "Making Impact"];
+const texts = ["Software Engineering", "Full Stack Development", "Constantly Growing", "Making Impact"];
 let index = 0;
 
 function changeText() {
@@ -13,3 +13,37 @@ function changeText() {
 
 setInterval(changeText, 3000);
 changeText(); 
+
+
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  var headerItems = document.getElementById("header-items");
+  headerItems.classList.toggle("show");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeElems = document.querySelectorAll(".fade-in");
+
+  const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px",
+  };
+
+  const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("active");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+
+  fadeElems.forEach((fadeElem) => {
+    appearOnScroll.observe(fadeElem);
+  });
+});
